6d7eab7755995adbe046058b0094b378610d5031
hrudyaharidas <hrudyaharidas002@gmail.com>
2023-03-17 23:42:54 +0400

    dashboard1

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+ @Output() isSideBarToggle=new EventEmitter()
+  sidebarToggle(){
+     this.isSideBarToggle.emit()
+     setTimeout(()=>{
+      window.dispatchEvent(
+        new Event('resize')
+      )
+     },300)
+  }
+}
